7b0e7fd85c209bd046739c96f9516505fb9d2206
tianyishinidie <32159729+tianyishinidie@users.noreply.github.com>
2019-11-28 10:45:32 +0800

    窗口文档和数据库SQL语句

--- 数据库表SQL.sql
+++ 数据库表SQL.sql
@@ -1,7 +1,25 @@
 create table userinfo
 (
-	account int not null,
+	id int not null,
 	nick varchar(20) not null,
 	password varchar(20) not null,
 	primary key(account)
+);
+
+create table friend
+(
+	toid int not null,
+	fromid int not null,
+	stat int not null,
+	primary key(toaccount,fromaccount)
+);
+
+create table chathistory
+(
+	fromid int not null,
+	toid int not null,
+	sendtime datetime not null,
+	content varchar(200) not null,
+	contenttype int not null,
+	primary key(fromid,toid,sendtime)
 );
